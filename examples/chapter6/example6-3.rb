a7373cf62d3b8872c171d127ecbedcd305d22053
Kensuke Kaneko <kyanny@gmail.com>
2010-01-21 03:33:27 +0900

    例6-3

---
+++ examples/chapter6/example6-3.rb
@@ -0,0 +1,14 @@
+def next_of(origin)
+    value = origin + 1
+    p value
+end
+def prev_of(origin)
+    value = origin - 1
+    p value
+end
+
+next_of(2) #=> 3
+next_of(2) #=> 3
+prev_of(2) #=> 1
+p value #=> error
+p undefined_variable #=> error
