16fd49b01f4dc39c82f650b2b7add74e0f174791
Kensuke Kaneko <kyanny@gmail.com>
2010-01-21 20:38:21 +0900

    例7-5

---
+++ examples/chapter7/example7-5.rb
@@ -0,0 +1,13 @@
+def sum(x, y)
+    x + y
+end
+def diff x, y
+    x - y
+end
+def prod(x, y) x * y end
+def quo x, y; x / y end
+
+p sum(1, 2) #=> 3
+p diff(1, 2) #=> -1
+p prod(1, 2) #=> 2
+p quo(1, 2) #=> 0
